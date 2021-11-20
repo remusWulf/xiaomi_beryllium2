13cbdfa374b5316eb3fb2a32155ee27a7c62262d
remusWulf <remuswulf@hotmail.com>
2021-11-20 12:30:20 +0000

    Fix building of GAPPS

--- arrow_beryllium.mk
+++ arrow_beryllium.mk
@@ -14,7 +14,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 # GApps
 TARGET_GAPPS_ARCH := arm64
 $(call inherit-product, vendor/google/pixel/config.mk)
-$(call inherit-product, vendor/google/gms/config.mk)
+$(call inherit-product, vendor/google/gms/products/gms.mk)
 
 # Device identifier. This must come after all inclusions.
 DEVICE_MAINTAINER := XtremeOrnob
